f27077f4d0b5c30c61de3662890aceb15ae3dec2
ryunosuke.yamamoto <ryunosukeyamamoto0@gmail.com>
2020-11-05 10:15:49 +0000

    favorite

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,15 @@
+class FavoritesController < ApplicationController
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favo(micropost)
+    flash[:success] = '投稿をお気に入りしました。'
+    redirect_to root_url
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavo(micropost)
+    flash[:success] = '投稿のお気に入りを解除しました。'
+    redirect_to root_url
+  end
+end
